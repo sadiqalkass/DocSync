f26ad2645df8675fc24f75c6fa9e09df2296c402
Sadiq Dayyub <saddayyub@gmail.com>
2026-07-07 17:02:46 +0100

    feat: initialize express server with database, cloudinary, and modular routing configurations

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,10 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(port, ()=> console.log("Server running at port " + port ))
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
 
 export default app;
